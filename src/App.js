1e4d638edb2e3fb221575f6ff36df2589ec62797
rickyalmeida <ricky.almeida@rxpro.com.br>
2021-11-22 22:54:34 -0300

    add status bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import {View} from 'react-native';
+import {StatusBar, View} from 'react-native';
 import tw, {withDeviceContext} from 'tw';
 import {Header, TodoInput, TodosList} from '@app/components';
 
 const App = () => (
   <View style={tw`flex-1 bg-light`}>
+    <StatusBar
+      backgroundColor={tw`text-primary`.color}
+      barStyle="light-content"
+    />
     <Header />
     <View style={tw`px-6 -mt-7`}>
       <TodoInput />
